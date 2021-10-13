1726c1092b9936ea8cbb40eb54d61cb6d9a774cd
USER <USER@ST3-U455-01R>
2021-10-13 17:52:51 +0300

    Добавьте файлы проекта.

---
+++ CarPage/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CarPage
+{
+    public partial class MainPage : CarouselPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
